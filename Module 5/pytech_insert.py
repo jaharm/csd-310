ac235916b27e9ff2eb9e61d77beb93f1a9cc55e6
jaharm <95267241+jaharm@users.noreply.github.com>
2022-09-11 21:27:50 -0500

    Update pytech_insert.py
    Assignment 5.3 corrected

--- Module 5/pytech_insert.py
+++ Module 5/pytech_insert.py
@@ -5,20 +5,90 @@ from pymongo import MongoClient
 url = "mongodb+srv://python:Mu3J8Ul3FDuiQJtU@cluster0.ush2cwx.mongodb.net/?retryWrites=true&w=majority"
 client = MongoClient(url)
 db = client.pytech
+
+""" three student documents"""
+
 jane = {
-    'student_id': 1007,
-    'first_name': 'Jane',
-    'last_name': 'Austen'
+    "student_id": "1007",
+    "first_name": "Jane",
+    "last_name": "Austen",
+    "enrollments": [
+        {
+            "term": "Session 2",
+            "gpa": "4.0",
+            "start_date": "July 10, 2020",
+            "end_date": "September 14, 2020",
+            "courses": [
+                {
+                    "course_id": "CSD310",
+                    "description": "Database Development and Use",
+                    "instructor": "Professor",
+                    "grade": "A+"
+                },
+                {
+                    "course_id": "CSD320",
+                    "description": "Programming with Java",
+                    "instructor": "Professor",
+                    "grade": "A+"
+                }
+            ]
+        }
+    ]
+
 }
 george = {
-    'student_id': 1008,
-    'first_name': 'George',
-    'last_name': 'Elliot'
+    "student_id": "1008",
+    "first_name": "George",
+    "last_name": "Elliot",
+    "enrollments": [
+        {
+            "term": "Session 2",
+            "gpa": "3.52",
+            "start_date": "July 10, 2020",
+            "end_date": "September 14, 2020",
+            "courses": [
+                {
+                    "course_id": "CSD310",
+                    "description": "Database Development and Use",
+                    "instructor": "Professor",
+                    "grade": "B+"
+                },
+                {
+                    "course_id": "CSD320",
+                    "description": "Programming with Java",
+                    "instructor": "Professor",
+                    "grade": "A-"
+                }
+            ]
+        }
+    ]
 }
 anne = {
-    'student_id': 1009,
-    'first_name': 'Anne',
-    'last_name': 'Bronte'
+    "student_id": "1009",
+    "first_name": "Anne",
+    "last_name": "Bronte",
+    "enrollments": [
+        {
+            "term": "Session 2",
+            "gpa": "1.5",
+            "start_date": "July 10, 2020",
+            "end_date": "September 14, 2020",
+            "courses": [
+                {
+                    "course_id": "CSD310",
+                    "description": "Database Development and Use",
+                    "instructor": "Professor",
+                    "grade": "C"
+                },
+                {
+                    "course_id": "CSD 320",
+                    "description": "Programming with Java",
+                    "instructor": "Professor Krasso",
+                    "grade": "B"
+                }
+            ]
+        }
+    ]
 }
 result_jane = db.students.insert_one(jane).inserted_id
 result_george = db.students.insert_one(george).inserted_id
